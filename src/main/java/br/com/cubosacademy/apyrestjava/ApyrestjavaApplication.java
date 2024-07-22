62af2ef3cac8a84ca3e8299d485933fc9118f9de
William B. Pereira <braswilliam@gmail.com>
2024-07-22 04:16:26 -0300

    base project created.

---
+++ src/main/java/br/com/cubosacademy/apyrestjava/ApyrestjavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.cubosacademy.apyrestjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApyrestjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApyrestjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cubosacademy/apyrestjava/ApyrestjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cubosacademy.apyrestjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApyrestjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
