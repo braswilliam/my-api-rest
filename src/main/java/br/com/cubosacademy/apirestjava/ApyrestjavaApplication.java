333332ae51e8de41f8b2e5cdad9032761a86956f
William B. Pereira <braswilliam@gmail.com>
2024-07-23 05:57:17 -0300

    application.properties conection for  database.

--- src/main/java/br/com/cubosacademy/apirestjava/ApyrestjavaApplication.java
+++ src/main/java/br/com/cubosacademy/apirestjava/ApyrestjavaApplication.java
@@ -1,4 +1,4 @@
-package br.com.cubosacademy.apyrestjava;
+package br.com.cubosacademy.apirestjava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
